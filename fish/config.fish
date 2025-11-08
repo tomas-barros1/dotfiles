4c09d6c7505f7ba25bebf5e0bcec20fd0f9a2a3c
tomas-barros1 <tomasabbarros3@gmail.com>
2025-11-08 13:01:39 -0300

    vs code settings, fish settings updated

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,14 @@
-# Prompt / UI
 starship init fish | source
 
-# Environment
 set -x EDITOR nvim
 set -x SUDO_EDITOR $EDITOR
 
-# fzf: options for ctrl-t (use in other tools that read this variable)
 set -x FZF_CTRL_T_OPTS "\
   --style full\
   --walker-skip .git,node_modules,target\
   --preview 'bat -n --color=always {}'\
   --bind 'ctrl-/:change-preview-window(down|hidden)'"
 
-# Functions
 function convert
     set input $argv[1]
     set output (string replace -r '\.mp4$' '.mkv' $input)
@@ -25,7 +21,6 @@ function fastconvert
     ffmpeg -i $input -c copy $output
 end
 
-# Aliases
 alias l="ls -la"
 alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
 alias cat="bat --theme='CatppuccinMocha' --paging=never"
