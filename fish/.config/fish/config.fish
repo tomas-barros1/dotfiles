2bbcbae05a446f3b825a902bbdf7fc9f9a73e300
tomas-barros1 <tomasabbarros3@gmail.com>
2025-11-18 17:59:26 -0300

    fixes, change starship to oh-my-posh

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,4 @@
-starship init fish | source
-
+set --export --global POSH_SESSION_ID "8ed82ee3-34f7-4c26-b9a4-6363650f9ab4"
 set -x EDITOR nvim
 set -x SUDO_EDITOR $EDITOR
 
@@ -23,5 +22,9 @@ end
 
 alias l="ls -la"
 alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
-alias cat="bat --theme='CatppuccinMocha' --paging=never"
+alias z="zoxide"
+alias cat="bat --theme='Catppuccin Mocha' --paging=never"
+
 ~/.local/bin/mise activate fish | source
+zoxide init fish | source
+source '/home/tom/.cache/oh-my-posh/init.12353866967644111694.fish'
