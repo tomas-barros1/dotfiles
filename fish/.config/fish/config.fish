c8774f2796cef25a3b6c69721c8cf6fbda9996e2
tomas-barros1 <tomasabbarros3@gmail.com>
2026-02-16 16:33:48 -0300

    fish changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,19 +2,20 @@ set -x EDITOR nvim
 set -x SUDO_EDITOR $EDITOR
 
 set -x FZF_CTRL_T_OPTS "\
-  --style full\
-  --walker-skip .git,node_modules,target\
-  --preview 'bat -n --color=always {}'\
+  --style full \
+  --walker-skip .git,node_modules,target \
+  --preview \"bat -n --theme='Catppuccin Mocha' --color=always {}\" \
   --bind 'ctrl-/:change-preview-window(down|hidden)'"
 
 alias y="yay"
 alias l="ls -la"
 alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
 alias z="zoxide"
-alias cat="bat --theme='Catppuccin Mocha' --paging=never"
+alias cat="bat --theme='Catppuccin Mocha' --paging=never --color=always"
 alias pg="docker exec -it -u postgres postgres psql"
 
 zoxide init fish | source
 starship init fish | source
 /home/tom/.local/bin/mise activate fish | source
 atuin init fish --disable-up-arrow | source
+fzf --fish | source
