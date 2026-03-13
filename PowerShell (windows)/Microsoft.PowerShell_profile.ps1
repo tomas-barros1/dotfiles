34dd076559c2c897b9dc69b03e6da0c144fd9e9e
Tomás Barros <tomasabbarros3@gmail.com>
2026-03-13 18:18:16 -0300

    Update PowerShell profile with new configurations

--- PowerShell (windows)/Microsoft.PowerShell_profile.ps1
+++ PowerShell (windows)/Microsoft.PowerShell_profile.ps1
@@ -1,24 +1,13 @@
-# Desativa telemetria e verificação de update
 $env:POWERSHELL_TELEMETRY_OPTOUT = 0
 $env:POWERSHELL_UPDATECHECK = 0
 
-# Carrega o prompt do Oh My Posh a partir do cache
-if (Test-Path "$env:USERPROFILE\.omp_init.ps1") {
-    . "$env:USERPROFILE\.omp_init.ps1"
-}
+Invoke-Expression (& 'C:\Users\Tom\AppData\Local\Microsoft\WinGet\Links\starship.exe' init powershell --print-full-init | Out-String)
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
-# Ativar Previsões de Comando (autocomplete avançado)
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView
 
-# Melhorar a experiência de autocompletar
 Set-PSReadLineOption -EditMode Windows
 Set-PSReadLineKeyHandler -Chord Tab -Function Complete
 
-# Ativar ícones no terminal
-Import-Module -Name Terminal-Icons -ErrorAction SilentlyContinue
-
-# Função para abrir o WSL direto na home
-function wsl {
-    & "C:\WINDOWS\system32\wsl.exe" -d Arch --cd ~
-}
+Import-Module -Name Terminal-Icons
