61477e1d1faf07279182f5c7967e791b544318c1
Tomás Barros <tomasabbarros3@gmail.com>
2026-03-20 13:20:02 -0300

    Set PowerShell 7 as default shell in wezterm config
    Updated wezterm configuration to set PowerShell 7 as the default shell and modified font settings.

--- wezterm/.config/.wezterm.lua
+++ wezterm/.config/.wezterm.lua
@@ -1,22 +1,9 @@
 local wezterm = require("wezterm")
 
-local function file_exists(path)
-    local f = io.open(path, "r")
-    if f ~= nil then io.close(f) return true else return false end
-end
-
 local config = {
-    audible_bell = "Disabled",
-    check_for_updates = false,
     color_scheme = "catppuccin-mocha",
-    inactive_pane_hsb = {
-        hue = 1.0,
-        saturation = 1.0,
-        brightness = 1.0,
-    },
-    font = wezterm.font("Hack Nerd Font"),
+    font = wezterm.font("CaskaydiaCove Nerd Font"),
     font_size = 12.0,
-    launch_menu = {},
     leader = { key = "a", mods = "CTRL" },
     disable_default_key_bindings = true,
     keys = {
@@ -56,46 +43,7 @@ local config = {
         { key = "DownArrow",  mods = "CTRL", action = wezterm.action.AdjustPaneSize { "Down", 5 },  repeatable = true },
     },
     set_environment_variables = {},
+	default_prog = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe", "-NoLogo" }
 }
-
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-    -- Define PowerShell 7 como shell padrão
-    config.default_prog = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe" }
-
-    -- Adiciona PowerShell 7 ao menu de inicialização
-    table.insert(config.launch_menu, {
-        label = "PowerShell 7",
-        args = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe" },
-    })
-
-    -- Adiciona também o PowerShell legado (opcional)
-    table.insert(config.launch_menu, {
-        label = "PowerShell (Legacy)",
-        args = { "powershell.exe", "-NoLogo" },
-    })
-
-    -- Detecta VS Tools e adiciona ao menu
-    for _, vsvers in ipairs(wezterm.glob("Microsoft Visual Studio/20*", "C:/Program Files (x86)")) do
-        local year = vsvers:gsub("Microsoft Visual Studio/", "")
-        table.insert(config.launch_menu, {
-            label = "x64 Native Tools VS " .. year,
-            args = {
-                "cmd.exe",
-                "/k",
-                "C:/Program Files (x86)/" .. vsvers .. "/BuildTools/VC/Auxiliary/Build/vcvars64.bat"
-            },
-        })
-    end
-else
-    local fish_bin_path = "/bin/fish"
-    if file_exists("/opt/homebrew/bin/fish") then
-        fish_bin_path = "/opt/homebrew/bin/fish"
-        config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
-    else
-        config.default_prog = { '/bin/bash', '-l' }
-    end
-    table.insert(config.launch_menu, { label = "fish", args = { fish_bin_path, "-l" } })
-    table.insert(config.launch_menu, { label = "bash", args = { "bash", "-l" } })
-end
 
 return config
