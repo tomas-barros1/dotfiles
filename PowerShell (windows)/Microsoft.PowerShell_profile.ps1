0fd90acb1a0b6f7606c173e4fe6a9816b7a0212b
Tomás Barros <tomasabbarros3@gmail.com>
2026-03-20 13:23:27 -0300

    Refactor PowerShell profile for enhanced user experience
    Updated PowerShell profile settings for improved usability and added PSReadLine options.

--- PowerShell (windows)/Microsoft.PowerShell_profile.ps1
+++ PowerShell (windows)/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,26 @@
-$env:POWERSHELL_TELEMETRY_OPTOUT = 0
+$env:POWERSHELL_TELEMETRY_OPTOUT = 1
 $env:POWERSHELL_UPDATECHECK = 0
 
 Invoke-Expression (& 'C:\Users\Tom\AppData\Local\Microsoft\WinGet\Links\starship.exe' init powershell --print-full-init | Out-String)
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -PredictionViewStyle ListView
-
-Set-PSReadLineOption -EditMode Windows
-Set-PSReadLineKeyHandler -Chord Tab -Function Complete
-
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-
-Import-Module -Name Terminal-Icons
+Import-Module Terminal-Icons
 Import-Module PSFzf
+
+# PSReadLine estilo fish
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle InlineView
+
+Set-PSReadLineOption -EditMode Emacs
+
+Set-PSReadLineKeyHandler -Chord RightArrow -Function AcceptSuggestion
+Set-PSReadLineKeyHandler -Chord Ctrl+RightArrow -Function ForwardWord
+
+Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
+
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -MaximumHistoryCount 10000
+
+Set-PsFzfOption `
+ -PSReadlineChordProvider 'Ctrl+t' `
+ -PSReadlineChordReverseHistory 'Ctrl+r'
