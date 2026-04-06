fa0872d1d20994dd6f88f6d3e5076e3c869cc910
Tomás Barros <tomasabbarros3@gmail.com>
2026-04-06 11:29:20 -0300

    Update Microsoft.PowerShell_profile.ps1 with new features
    Refactor PowerShell profile to enhance navigation and prompt display.

--- PowerShell (windows)/Microsoft.PowerShell_profile.ps1
+++ PowerShell (windows)/Microsoft.PowerShell_profile.ps1
@@ -7,13 +7,9 @@ Invoke-Expression (& { (zoxide init powershell | Out-String) })
 Import-Module Terminal-Icons
 Import-Module PSFzf
 
-# PSReadLine estilo fish
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle InlineView
 
-Set-PSReadLineOption -EditMode Emacs
-
-Set-PSReadLineKeyHandler -Chord RightArrow -Function AcceptSuggestion
 Set-PSReadLineKeyHandler -Chord Ctrl+RightArrow -Function ForwardWord
 
 Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
@@ -21,6 +17,38 @@ Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 Set-PSReadLineOption -MaximumHistoryCount 10000
 
+Set-PSReadLineKeyHandler -Key Alt+LeftArrow  -Function BackwardWord
+Set-PSReadLineKeyHandler -Key Alt+RightArrow -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord Alt+Backspace -Function BackwardKillWord
+
 Set-PsFzfOption `
  -PSReadlineChordProvider 'Ctrl+t' `
  -PSReadlineChordReverseHistory 'Ctrl+r'
+
+Remove-Item Alias:cd -Force -ErrorAction SilentlyContinue
+
+function cd {
+    if ($args.Count -eq 0) {
+        Set-Location ~
+        return
+    }
+
+    $path = $args -join " "
+
+    if (Test-Path $path) {
+        Set-Location $path
+    } else {
+        z $path
+    }
+}
+
+$prompt = ""
+function Invoke-Starship-PreCommand {
+    $current_location = $executionContext.SessionState.Path.CurrentLocation
+    if ($current_location.Provider.Name -eq "FileSystem") {
+        $ansi_escape = [char]27
+        $provider_path = $current_location.ProviderPath -replace "\\", "/"
+        $prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
+    }
+    $host.ui.Write($prompt)
+}
