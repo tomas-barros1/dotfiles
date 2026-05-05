37a2d28ebfa2e05d32ae702c2c63222f68ce8a47
tomas-barros1 <tomasabbarros3@gmail.com>
2026-05-05 14:10:38 -0300

    *

--- astro-nvim/.config/nvim/lua/community.lua
+++ astro-nvim/.config/nvim/lua/community.lua
@@ -7,4 +7,5 @@ return {
   { import = "astrocommunity.pack.ruby" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.go" }
 }
