0c265281bb3adaf8e38626b1a733c52b9e2954e9
tomas-barros1 <tomasabbarros3@gmail.com>
2026-08-18 19:51:15 -0300

    chore(hypr): clean autostart for uwsm session management

--- hypr/.config/hypr/autostart.lua
+++ hypr/.config/hypr/autostart.lua
@@ -1,6 +1,4 @@
 hl.on("hyprland.start", function()
-	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP DISPLAY DBUS_SESSION_BUS_ADDRESS")
-	hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP DISPLAY DBUS_SESSION_BUS_ADDRESS")
 	hl.exec_cmd("/usr/bin/gnome-keyring-daemon --start --components=secrets,pkcs11,ssh")
 	hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
 	hl.exec_cmd("fcitx5 -d")
