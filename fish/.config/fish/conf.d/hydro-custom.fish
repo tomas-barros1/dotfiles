159010143bf1aa5d2d685a1ecf0dc6161714414d
tomas-barros1 <tomasabbarros3@gmail.com>
2026-09-07 15:13:20 -0300

    fish: separar overrides hydro do arquivo do plugin (hydro-custom.fish)

---
+++ fish/.config/fish/conf.d/hydro-custom.fish
@@ -0,0 +1,10 @@
+# Overrides de cores e opções do Hydro prompt.
+# Mantidos em arquivo separado (nao conf.d/hydro.fish) para nao serem
+# sobrescritos por um update do plugin hydro.
+set --global hydro_color_duration F9E2AF
+set --global hydro_color_error F38BA8
+set --global hydro_color_git CBA6F7
+set --global hydro_color_prompt CBA6F7
+set --global hydro_color_pwd 89B4FA
+set --global hydro_color_start A6E3A1
+set --global hydro_multiline true
