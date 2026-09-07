0f917455d5edbf31f56e4a6178074f6ef66aab3d
tomas-barros1 <tomasabbarros3@gmail.com>
2026-09-07 15:36:15 -0300

    fish: mover overrides do hydro para hydro_overrides.fish

---
+++ fish/.config/fish/conf.d/hydro_overrides.fish
@@ -0,0 +1,7 @@
+set -g hydro_color_duration F9E2AF
+set -g hydro_color_error F38BA8
+set -g hydro_color_git CBA6F7
+set -g hydro_color_prompt CBA6F7
+set -g hydro_color_pwd 89B4FA
+set -g hydro_color_start A6E3A1
+set -g hydro_multiline true
